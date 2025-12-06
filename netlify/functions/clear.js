1e7560e5a1df9cb65a17d59e2212247fd950e817
qtdu <116353460+qtdu@users.noreply.github.com>
2025-12-06 21:07:57 +0700

    Delete netlify/functions directory

--- netlify/functions/clear.js
+++
@@ -1,5 +0,0 @@
-export default async function handler() {
-  const kv = await import("@netlify/kv");
-  await kv.delete("mockjson");
-  return new Response("Cleared OK");
-}
--- netlify/functions/load.js
+++
@@ -1,8 +0,0 @@
-export default async function handler() {
-  const kv = await import("@netlify/kv");
-  const data = await kv.get("mockjson");
-
-  return new Response(JSON.stringify(data || {}), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
